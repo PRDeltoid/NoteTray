c6d508e3f099cdead31628e933dbfee91226f88a
Taylor Britton <tbrit10@gmail.com>
2022-05-16 14:24:10 -0700

    Add NoteTrayLib project
    - Add a new project to host shared library functions
    - Add Directory service for getting information about a directory and its contents

---
+++ NoteTrayLib/DirectoryService.cs
@@ -0,0 +1,22 @@
+﻿namespace NoteTrayLib;
+
+public class DirectoryService
+{
+    public IEnumerable<string> GetChildDirectories(string basePath)
+    {
+        return Directory.GetDirectories(basePath).Where(x =>
+        {
+            var fileInfo = new FileInfo(x);
+            return fileInfo.Attributes.HasFlag(FileAttributes.Hidden) == false;
+        }).Select(Path.GetFileName).Where(x => x != null)!;
+    }
+
+    public IEnumerable<string> GetChildFiles(string basePath)
+    {
+        return Directory.GetFiles(basePath).Where(x =>
+        {
+            var fileInfo = new FileInfo(x);
+            return fileInfo.Attributes.HasFlag(FileAttributes.Hidden) == false;
+        }).Select(Path.GetFileName).Where(x => x != null)!;
+    }
+}
