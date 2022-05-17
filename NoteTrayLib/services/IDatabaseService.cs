e22cd34dd1e1bf5c6718fb7a6130886d368ad9a8
Taylor Britton <tbrit10@gmail.com>
2022-05-16 18:30:05 -0700

    Add Database service
    - Add DatabaseService interface
    - Add SQLite implementation for DatabaseService interface

---
+++ NoteTrayLib/services/IDatabaseService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace NoteTrayLib.services;
+
+public interface IDatabaseService
+{
+    int ExecuteNonQuery(string commandString, object parameters = null);
+    IEnumerable<T> ExecuteQuery<T>(string commandString, object parameters = null);
+}
---
+++ NoteTrayLib/services/SQLiteDatabaseService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using Dapper;
+using Serilog;
+
+namespace NoteTrayLib.services;
+
+public class SQLiteDatabaseService : IDatabaseService
+{
+    private readonly string _databaseFile;
+
+    public SQLiteDatabaseService(string databaseFileName)
+    {
+        _databaseFile = databaseFileName;
+    }
+
+    public int ExecuteNonQuery(string commandString, object parameters = null)
+    {
+        try
+        {
+            using SQLiteConnection con = new SQLiteConnection($"Data Source={_databaseFile}");
+            con.Open();
+            return con.Execute(commandString, parameters);
+        }
+        catch (Exception e)
+        {
+            Log.Error("Exception: {e}", e);
+            return 0;
+        } 
+    }
+    
+    public IEnumerable<T> ExecuteQuery<T>(string commandString, object parameters = null)
+    {
+        try
+        {
+            using SQLiteConnection con = new SQLiteConnection($"Data Source={_databaseFile}");
+            con.Open();
+
+            return con.Query<T>(commandString, parameters);
+        }
+        catch (Exception e)
+        {
+            Log.Error("Exception: {e}", e);
+            return Enumerable.Empty<T>();
+        } 
+    }
+}
