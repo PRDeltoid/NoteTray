2b125c2145f6e8e3ce4ad0ea4b41438c92ca3e3a
Taylor Britton <tbrit10@gmail.com>
2022-05-20 21:18:07 -0700

    Add FirstTimeSetup service
    - Add a service to perform a setup one time and then set a flag in the database so that future sessions will not run the same setup. Useful for the long first time indexing of the notes directory

---
+++ NoteTrayLib/Services/FirstTimeSetupService.cs
@@ -0,0 +1,38 @@
+﻿using Serilog;
+
+namespace NoteTrayLib.Services;
+
+public class FirstTimeSetupService
+{
+    private readonly UserPreferenceService _userPrefs;
+    private bool _firstRun;
+
+    public FirstTimeSetupService(UserPreferenceService userPrefs)
+    {
+        _userPrefs = userPrefs;
+        if (_userPrefs.TryGetPreference<bool>("firstRun", out _firstRun) == false)
+        {
+            _userPrefs.SetPreference("firstRun", true);
+            _firstRun = true;
+        }; 
+    }
+    
+    public void Setup()
+    {
+        if (_firstRun)
+        {
+            Log.Debug("Performing first time setup");
+            // Disable future "first runs"
+            _userPrefs.SetPreference("firstRun", false);
+            // Prevent any further calls this session
+            _firstRun = false;
+        }
+    }
+
+    public void Reset()
+    {
+        Log.Debug("Resetting first time setup status");
+        _userPrefs.SetPreference("firstRun", true);
+        _firstRun = true; 
+    }
+}
