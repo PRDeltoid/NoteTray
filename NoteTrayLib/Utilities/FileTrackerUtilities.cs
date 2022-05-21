d69501c637b914a15b19d3dd43d014ff0de93cd7
Taylor Britton <tbrit10@gmail.com>
2022-05-20 17:31:21 -0700

    Add helper utility for FileTrackerService
    - Add a helper utility to make creating TrackedFileModel structures easier for use with the FileTrackerService

---
+++ NoteTrayLib/Utilities/FileTrackerUtilities.cs
@@ -0,0 +1,17 @@
+﻿using NoteTrayLib.Models;
+
+namespace NoteTrayLib.Utilities;
+
+public static class FileTrackerUtilities
+{
+    public static TrackedFileModel TrackedFileModelFromPath(string fullPath)
+    {
+        FileInfo fileInfo = new FileInfo(fullPath);
+        return new TrackedFileModel()
+        {
+            FileName = Path.GetFileName(fullPath),
+            Path = Path.GetDirectoryName(fullPath),
+            LastChanged = fileInfo.LastWriteTime
+        };
+    }
+}
