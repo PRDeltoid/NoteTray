215b6139d850bead265d5c7e4a7c04c2968b04e2
Taylor Britton <tbrit10@gmail.com>
2022-05-21 16:32:07 -0700

    Implement basic search in MainWindow

--- NoteTray/MainWindow.xaml.cs
+++ NoteTray/MainWindow.xaml.cs
@@ -16,15 +16,11 @@ namespace NoteTray
         private readonly IFullTextSearchService _searchService;
         private readonly WindowSnapper _windowSnapper;
         
-        public MainWindow(NoteListViewModel viewModel, StartupScanService startupScanner, FirstTimeSetupService firstTimeSetup, IFullTextSearchService searchService)
+        public MainWindow(NoteListViewModel viewModel, IFullTextSearchService searchService)
         {
-            _searchService = searchService;
-            // Initial setup
-            // TODO: Should either one of these be moved elsewhere? AutoActivate maybe? StartUp glue class with autoactivate?
-            firstTimeSetup.Setup();
-            startupScanner.Scan();
-            
             DataContext = viewModel;
+
+            _searchService = searchService;
             // Window Snapper is used to attach this window to the side of another process and keep it there
             // This is used when the ViewModel opens an editor process
             _windowSnapper = new WindowSnapper(this);
@@ -39,7 +35,7 @@ namespace NoteTray
             if (e.PropertyName != "EditorProcess") return;
             if (sender is not NoteListViewModel viewModel) return;
             
-            Log.Debug("Attaching to editor process: {0}",viewModel.EditorProcess.MainWindowTitle);
+            Log.Debug("Attaching to editor process: {0}", viewModel.EditorProcess.MainWindowTitle);
             _windowSnapper.Attach(viewModel.EditorProcess.MainWindowHandle);
         }
 
@@ -56,20 +52,21 @@ namespace NoteTray
         private void Settings_MouseLeftDown(object sender, MouseButtonEventArgs e)
         {
             if (sender is not Button button) return;
-            
+
             // Manually load context since this isn't done automatically when left clicking
             button.ContextMenu.DataContext = button.DataContext;
             // Show context menu
             button.ContextMenu.IsOpen = true;
         }
         
-        private void Search_EnterPressed(object sender, System.Windows.Input.KeyEventArgs e)
+        private void Search_EnterPressed(object sender, KeyEventArgs e)
         {
-            if (e.Key != Key.Enter) return;
+            if (e.Key != Key.Enter || txtSearchbox.Text == "") return;
 
             e.Handled = true;
             Log.Debug("Enter pressed in search box");
             
+            // MainWindow just starts the search. Handling the results is the job of the NoteListViewModel
             _searchService.Search(txtSearchbox.Text);
         }
     }
